c5994e19436d4b0966b93242b10715588d0378d1
Dorivika <avivek796@gmail.com>
2024-04-12 23:11:32 -0400

    POW algo

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,7 @@
-use sha2::{Digest, Sha256};
+mod block;
+mod proof;
 
-#[derive(Debug)]
-pub struct Block {
-    hash: Option<String>,
-    data: Option<String>,
-    prev_hash: Option<String>,
-}
-
-#[derive(Debug)]
-struct BlockChain {
-    blocks : Vec<Block>,
-}
-
-impl BlockChain {
-    pub fn AddBlock(& mut self, data : Option<String>){
-       let  prev_block = &self.blocks[self.blocks.len() -1];
-       let new_block = Block::create_block(data, prev_block.hash.clone());
-       self.blocks.push(new_block)
-    }
-
-    fn Genesis() -> Block {
-        Block::create_block(Some("Genesis".to_string()), Some("".to_string()))
-    }
-
-    pub fn init_blockchain() -> BlockChain {
-        let mut new_blockchain = vec![];
-        new_blockchain.push(BlockChain::Genesis());
-        BlockChain {
-            blocks : new_blockchain,
-        }
-    }
-}
-
-
-impl Block {
-    pub fn create_block(data : Option<String>, prev_hash: Option<String>) -> Block {
-        let mut block = Block {
-            hash : None,
-            data,
-            prev_hash
-        };
-
-        block.derive_hash();
-        block
-    }
-
-    fn derive_hash(&mut self) {
-        let mut hasher = Sha256::new();
-
-        let serialized_data = format!(
-            "{},{},{}",
-            self.data.as_deref().unwrap_or(""),
-            self.hash.as_deref().unwrap_or(""),
-            self.prev_hash.as_deref().unwrap_or("")
-        );
-
-        hasher.update(serialized_data.as_bytes());
-
-        let hash_result = hasher.finalize();
-        self.hash = Some(format!("{:x}", hash_result));
-    }
-}
+use block::BlockChain;
 
 fn main() {
     let mut chain = BlockChain::init_blockchain();
@@ -68,10 +9,13 @@ fn main() {
     chain.AddBlock(Some("second Block".to_string()));
     chain.AddBlock(Some("Thrid Block".to_string()));
 
-    for blocks in chain.blocks {
-        println!(" Prev hash : {}", blocks.prev_hash.unwrap());
-        println!("Data : {}", blocks.data.unwrap());
-        println!("Hash : {}", blocks.hash.unwrap());
+    for blocks in &chain.blocks {
+        println!(" Prev hash : {}", blocks.prev_hash.as_ref().unwrap());
+        println!("Data : {}", blocks.data.as_ref().unwrap());
+        println!("Hash : {}", blocks.hash.as_ref().unwrap());
+        let pow = proof::new_proof(&blocks);
+        
+        println!("POW : {}", proof::validate(&pow));
 
     }
     
---
+++ src/proof.rs
@@ -0,0 +1,80 @@
+const DIFFICULTY:u32 = 10;
+
+
+use crate::block::Block;
+use sha2::{Digest, Sha256};
+
+use num::{bigint::{self, BigInt}, BigUint, Num};
+
+pub struct Pow<'a> {
+    Block: &'a Block,
+    Target: BigInt,
+}
+
+pub fn new_proof<'a>(b: &'a Block) -> Pow<'a> {
+    let target: BigInt = BigInt::from(1);
+    let target = target << (256 - DIFFICULTY);
+    Pow {
+        Block : b,
+        Target : target,
+    }
+}
+
+impl  <'a>Pow <'a> {
+    pub fn run(&self) -> (i32, String) {
+        let int_hash : BigInt;
+        let hash : String;
+    
+        let mut nonce = 0;
+    
+        while nonce < i32::MAX {
+            let data = Pow::init_data(self, nonce);
+    
+            let mut hasher = Sha256::new();
+            hasher.update(data.as_bytes());
+            let hash = hasher.finalize();
+            let hash_as_str = format!("{:x}",hash);
+            println!("hash : {} ", hash_as_str);
+    
+            let hash_bigint = BigInt::from_str_radix(&hash_as_str, 16).unwrap();
+    
+            // Now you can compare hash_bigint with p.Target
+            if hash_bigint < self.Target {
+                return (nonce, hash_as_str);
+            }
+    
+            nonce+=1
+        }
+        (0, "".to_string())
+    }
+    fn init_data (p : &'a Pow , nonce : i32) -> String {
+        let data = format!("{},{},{},{:?},{:?}", 
+        p.Block.prev_hash.as_deref().unwrap_or(""),
+        p.Block.data.as_deref().unwrap_or(""),
+        "".to_string(),
+        nonce.to_ne_bytes(),
+        DIFFICULTY.to_ne_bytes()
+        );
+        data
+    }
+    
+}
+
+
+
+pub fn validate(p : &Pow) -> bool {
+    let int_hash : BigInt;
+    let data = Pow::init_data(p, p.Block.nonce.unwrap());
+
+    let mut hasher = Sha256::new();
+    hasher.update(data);
+    let hash = hasher.finalize();
+
+    let hash_as_str = format!("{:x}", hash);
+    let hash = BigInt::from_str_radix(&hash_as_str, 16).unwrap();
+
+    if hash<p.Target {
+        return true
+    };
+    false
+}
