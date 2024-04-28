5a01522f5b3d3309f6a6f1be09288df12beccec3
Dorivika <avivek796@gmail.com>
2024-04-27 23:26:24 -0400

    wallet persistence

--- wallet/src/wallet.rs
+++ wallet/src/wallet.rs
@@ -1,41 +1,51 @@
 use std::ops::Deref;
 use std::rc::Rc;
-
 use ring::rand;
 use ring::signature::{self, EcdsaKeyPair, KeyPair};
+use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
 use ripemd::Ripemd160;
 
 const CHECKSUM_LEN : i32 = 4;
 const  VERSION: u8 = 0x00;
 
-
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Wallet {
-    key_pair: EcdsaKeyPair,
+    pkcs8_bytes: Rc<[u8]>,
 }
 
 impl Wallet {
     fn newkeypair() -> Self {
         let rng = rand::SystemRandom::new();
-        let pkcs8_bytes = EcdsaKeyPair::generate_pkcs8(&signature::ECDSA_P256_SHA256_FIXED_SIGNING, &rng)
+        let binding = EcdsaKeyPair::generate_pkcs8(&signature::ECDSA_P256_SHA256_FIXED_SIGNING, &rng)
             .expect("Failed to generate PKCS#8 document");
+        let pkcs8_bytes = binding
+            .as_ref();
+        
 
-        let key_pair = EcdsaKeyPair::from_pkcs8(&signature::ECDSA_P256_SHA256_FIXED_SIGNING, pkcs8_bytes.as_ref(), &rng)
-            .expect("Failed to generate key pair");
-
-        Self { key_pair }
+        Self { 
+            pkcs8_bytes: Rc::from(pkcs8_bytes)
+         }
     }
 
-    pub fn public_key(&self) -> &[u8] {
-        self.key_pair.public_key().as_ref()
+    fn key_pair(&self) -> EcdsaKeyPair {
+        let rng = rand::SystemRandom::new();
+        let key_pair = EcdsaKeyPair::from_pkcs8(&signature::ECDSA_P256_SHA256_FIXED_SIGNING, &self.pkcs8_bytes.deref(), &rng).expect("failed to create the ECDSA keypair");
+        key_pair
+    }
+
+    pub fn public_key<'a>(&'a self) -> Rc::<[u8]> {
+        let binding = self.key_pair();
+        let binding = binding.public_key().as_ref();
+        Rc::from(binding)
     }
 
     pub fn make_wallet() -> Wallet {
         Wallet::newkeypair()
     }
     
-    pub fn address(&self) -> Rc<[u8]> {
-        let pubhashkey = pub_key_hash(self.public_key());
+    pub fn address(&self) -> String {
+        let pubhashkey = pub_key_hash(self.public_key().as_ref());
         let mut versioned_hash : Vec<u8> = vec![];
         versioned_hash.push(VERSION);
         let mut fullhash : Vec<u8> = vec![];
@@ -45,7 +55,7 @@ impl Wallet {
         let checksum = checksum(Rc::from(versioned_hash));
         fullhash.extend_from_slice(&checksum);
 
-        return Rc::from(fullhash);
+        String::from_utf8_lossy(&fullhash).to_string()
     }
 
 
--- wallet/src/wallets.rs
+++ wallet/src/wallets.rs
@@ -1,11 +1,50 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, error::Error, fs::{File, OpenOptions}, io::{Read, Write}, ops::Deref};
+use ring::error;
+
 use crate::wallet::Wallet;
 struct  Wallets {
-    wallets : HashMap<String, Wallet>
+    wallets : Option<HashMap<String, Wallet>>
 }
 
 impl Wallets {
-    fn save_file(&self) {
+    fn save_file(&self) -> Result<(), Box<dyn std::error::Error>> {
+        let ser_wallet = bincode::serialize(&self.wallets)
+                                .expect("failed to serialze wallets ");
+        let mut file = OpenOptions::new().write(true).open(format!("saved_wallets"))
+        .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+        file.write_all(&ser_wallet)
+        .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+        Ok(())
+    }
+
+    fn load_file(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let mut file = File::open(format!("saved_wallets"))
+        .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+
+        let mut ser_wallets : Vec<u8> = Vec::new();
+
+        file.read_to_end(&mut ser_wallets)
+        .map_err(|e| Box::new(e) as Box<dyn Error>)?;
         
+        if ser_wallets.len() < 1{
+            self.wallets = Some(HashMap::new())
+        }
+        self.wallets = bincode::deserialize(&ser_wallets).expect("unable to derserialize wallets");
+        Ok(())
+    }
+    
+    fn create_wallets() -> Result<(), Box<dyn std::error::Error>> {
+        let mut wallets = Wallets {
+            wallets : None,
+        };
+        let err = wallets.load_file();
+        println!("{:?}", err);
+
+        Ok(())
+    }
+
+    fn get_wallet(&self, address: String) -> Wallet {
+        let ws = self.wallets.as_ref().unwrap().get(&address).unwrap().clone();
+        ws
     }
 }
